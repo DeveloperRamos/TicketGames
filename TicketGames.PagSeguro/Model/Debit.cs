2c2061cf5b7c050a4a78ecb183bed29580bd2ff4
Marcio Andre Ramos Correia <marcio.correia@terra.com.br>
2017-08-25 23:33:40 -0300

    Commit dos arquivos criados.

---
+++ TicketGames.PagSeguro/Model/Debit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uol.PagSeguro.Domain.Direct;
+
+namespace TicketGames.PagSeguro.Model
+{
+    public class Debit
+    {
+        public OnlineDebitCheckout MappingDebitCheckout()
+        {
+            // Instantiate a new checkout
+            OnlineDebitCheckout checkout = new OnlineDebitCheckout();
+
+
+
+
+            return new OnlineDebitCheckout();
+        }
+    }
+}
---
+++ TicketGames.PagSeguro/Session.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uol.PagSeguro.Domain;
+using Uol.PagSeguro.Exception;
+using Uol.PagSeguro.Resources;
+using Uol.PagSeguro.Service;
+
+namespace TicketGames.PagSeguro
+{
+    public class Session
+    {
+        public string Id { get; set; }
+
+        public void CreateSession()
+        {
+            bool isSandbox = true;
+            EnvironmentConfiguration.ChangeEnvironment(isSandbox);
+
+            try
+            {
+                AccountCredentials credentials = PagSeguroConfiguration.Credentials(isSandbox);
+
+                Uol.PagSeguro.Domain.Direct.Session result = SessionService.CreateSession(credentials);
+
+                this.Id = result.id;
+            }
+            catch (PagSeguroServiceException exception)
+            {
+                //Gravar Log do erro
+            }
+        }
+    }
+}
