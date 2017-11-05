4981fd641e8a1694562bc3fb56b3a8aa17dc448a
Marcio Andre Ramos Correia <marcio.correia@terra.com.br>
2017-11-05 01:18:33 -0200

    Página de carrinho

---
+++ TicketGames.API/Models/Order/Cart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TicketGames.API.Models.Order
+{
+    public class Cart
+    {
+        public long Id { get; set; }
+        public long ProductId { get; set; }
+        public string Product { get; set; }
+        public int Qtd { get; set; }
+        public decimal Price { get; set; }
+    }
+}
--- TicketGames.Web/app/appConfig.js
+++ TicketGames.Web/app/appConfig.js
@@ -11,6 +11,7 @@
         .when('/', { templateUrl: 'app/views/home.html', controller: 'homeController', controllerAs: 'vmHome' })
         .when('/Produto/:id', { templateUrl: 'app/views/product.html', controller: 'productController', controllerAs: 'vmProduct' })
         .when('/Busca', { templateUrl: 'app/views/search.html', controller: 'searchController', controllerAs: 'vmSearch' })
+        .when('/Carrinho', { templateUrl: 'app/views/cart.html', controller: 'cartController', controllerAs: 'vmCart' })
         //.when('/Acesso', { templateUrl: 'app/Views/Login.html', controller: 'LoginController', controllerAs: 'vmLogin' })        
         .otherwise({ redirectTo: '/' });
 }]);
