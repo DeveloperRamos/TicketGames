0688dd01342c48a9eb2bce3e0f6a7988103aeb38
Marcio Andre Ramos Correia <marcio.correia@terra.com.br>
2017-11-29 23:27:20 -0200

    Participant

--- TicketGames.API/Controllers/ParticipantController.cs
+++ TicketGames.API/Controllers/ParticipantController.cs
@@ -5,6 +5,12 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
+using TicketGames.API.Models.Participant;
+using TicketGames.CrossCutting.Cache;
+using TicketGames.CrossCutting.Cache.Redis;
+using TicketGames.Domain.Contract;
+using TicketGames.Domain.Services;
+using TicketGames.Infrastructure.Repositories;
 
 namespace TicketGames.API.Controllers
 {
@@ -12,6 +18,22 @@ namespace TicketGames.API.Controllers
     [ApiExplorerSettings(IgnoreApi = false)]
     public class ParticipantController : ApiController
     {
+        private readonly IParticipantService _participantService;
+        public ParticipantController(IParticipantService participantService)
+        {
+            this._participantService = participantService;
+        }
+        public ParticipantController()
+            : this(new ParticipantService(new ParticipantRepository()))
+        {
+            CacheManager.SetProvider(new CacheProvider());
+        }
 
+
+        [HttpPost]
+        public IHttpActionResult Post(Participant participant)
+        {
+            return Ok();
+        }
     }
 }
---
+++ TicketGames.API/Models/Participant/Participant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TicketGames.API.Models.Participant
+{
+    public class Participant
+    {
+        public long ParticipantId { get; set; }
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
