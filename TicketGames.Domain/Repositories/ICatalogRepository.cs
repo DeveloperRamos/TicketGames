6bd1cef1326ee2e5d0ab43be8754edb117139ed5
Marcio Andre Ramos Correia <marcio.correia@terra.com.br>
2017-12-27 23:53:14 -0200

    busca por palavra

--- TicketGames.Domain/Repositories/ICatalogRepository.cs
+++ TicketGames.Domain/Repositories/ICatalogRepository.cs
@@ -10,6 +10,7 @@ namespace TicketGames.Domain.Repositories
         List<Product> GetProducts(int categoryId);
         List<Product> GetProducts(string name);
         List<Product> GetRecentProductsByCategory(long categoryId);
+        List<Product> GetProducts(int categoryId, string name);
         Image CreateImage(Product product);
         Image UpdateImage(int typeImageId, long productId, string newUrl);
     }
